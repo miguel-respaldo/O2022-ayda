454dde9889bdea2e43696e15612c907be2744f19
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2022-08-29 19:37:26 -0500

    factorial

---
+++ 0829/290.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+def factorial(n):
+    return n*2
+
+numero = eval(input("Escribe un número: "))
+
+print("El factorial de", numero, "es", factorial(numero))
