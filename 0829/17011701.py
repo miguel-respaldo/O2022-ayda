e71a622eadf4c95a8ae88779248d31daa1efaa32
Christopher Esparza <christopher.esparza.contreras@gmail.com>
2022-08-29 19:50:45 -0500

    factorial 17011701

---
+++ 0829/17011701.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+def factorial(n):
+    if n == 0:
+        return 1
+    for i in range(1,n):
+        n = n * i
+    return n
+
+numero = eval(input("Escribe un número: "))
+
+print("El factorial de", numero, "es", factorial(numero))
