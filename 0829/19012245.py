42947fd9a0d7c5cb29b6d1d6d399b81af1cf6455
Pulopez22 <111822160+Pulopez22@users.noreply.github.com>
2022-08-31 18:29:34 -0500

    Create 19012245.py

---
+++ 0829/19012245.py
@@ -0,0 +1,13 @@
+# Nombre: Lopez Peter Ubaldo
+# No. Control: 19012245
+# Calificación: XXX
+
+
+from math import factorial
+
+num = eval(input("Escribe un numero =>"));
+
+if (num <0):
+    print("Numeros negaticos no existen")
+else: 
+    print ("El Factorial es", factorial(num))
