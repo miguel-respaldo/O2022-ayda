2dba25987e1586cc7f7d0550417d0199dd5ff33e
joserodriguez-pro <83359901+joserodriguez-pro@users.noreply.github.com>
2022-09-01 11:01:39 -0500

    factorial

---
+++ 0829/19011256.py
@@ -0,0 +1,14 @@
+print("Ingrese un numero ")
+num = int(input())
+facto = 1
+
+if num == 0:
+    	print("El factorial de 0 es 1")
+
+elif num>0:
+    for i in range(1, num+1):
+        facto*=i    	
+    print("El factorial de ",num,"es",facto)
+
+else:
+    print("Usted ingreso un numero negativo o un valor que no es un numero")
