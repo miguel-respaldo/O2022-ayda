41c09c2250fcb277e6349de4585af1e657022e8f
FranciscoOT8 <111822461+FranciscoOT8@users.noreply.github.com>
2022-09-02 17:49:08 -0500

    factorial.py

---
+++ 0829/factorial.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Nombre: Francisco Javier Ocampo Torres
+# No. Control: 18011167
+# Calificación: XXX
+
+def factorial(f):
+    if f<0:
+        return 0
+    elif f==0 or f==1:
+        return 1
+    else:
+        fact=1
+        while(f>1):
+            fact*=f
+            f-=1
+        return fact
+
+num=eval(input("Ingresa un numero: "))
+print("El factorial de ",num,"es",
+factorial(num))
