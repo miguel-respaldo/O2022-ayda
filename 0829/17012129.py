f8aed89f1df809e6f2cb7fa2593531690ef96390
richisiordia <richysiordia@gmail.com>
2022-09-02 13:30:29 -0500

    17012129

---
+++ 0829/17012129.py
@@ -0,0 +1,14 @@
+
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Nombre: José Ricardo Siordia Alatorre
+# No. Control: 17012129
+# Calificación: 
+from math import factorial
+
+print ("El Factorial de tu numero es", factorial(6))
+
