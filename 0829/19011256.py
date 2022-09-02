12565acabe4276c7ab1402de04d4aa9a22a7e6fd
joserodriguez-pro <83359901+joserodriguez-pro@users.noreply.github.com>
2022-09-02 17:32:38 -0500

    Update 19011256.py

--- 0829/19011256.py
+++ 0829/19011256.py
@@ -1,6 +1,12 @@
-#Alumno: Rodriguez Pacheco Jose Ruben
-#Numero De Control: 19011256
-#Calificacion:
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Nombre: Rodriguez Pacheco Jose Ruben
+# No. Control: 19011256
+# Calificación: XXX
 print("Ingrese un numero ")
 num = int(input())
 facto = 1
