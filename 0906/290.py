0ff83cde0f071a3f96a33fda41e03d97a33cd6d0
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2022-09-06 19:15:39 -0500

    factorial

---
+++ 0906/290.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+#
+# Nombre: Miguel Bernal Marin
+# No. Control: 290
+# Calificación: XXX
+
+def factorial(num):
+    # Solo modifican aqui
+    return 2*num # y aqui claro
+
+
+numero = eval(input("Escribe un número: "))
+print("El factorial de", numero, "es", factorial(numero))
---
+++ plantilla.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+#
+# Nombre: Nombre del Estudiante
+# No. Control: 123456789
+# Calificación: XXX
