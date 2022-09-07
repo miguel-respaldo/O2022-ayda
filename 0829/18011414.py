4391a852cccaa5924b44b34510dce26a2dc9a170
LeonardoRg18011414 <za18011414@zapopan.tecmm.edu.mx>
2022-09-07 14:08:59 -0500

    tarea

---
+++ 0829/18011414.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Nombre: Omar Ramses Lopez Pulido
+# No. Control: 18012180
+# Calificación: XXX
+def factorial(n):
+    resultado = 0
+    for i in range(0, n+1):
+        if i==0 or i==1:
+            resultado = 1
+        elif i > 1:
+            resultado = i * resultado
+    return resultado
+
+x = int(input("Ingrese un numero: "))
+if x < 0:
+    print("No existe el fatoral de: ", x)
+else:
+    fact = factorial(x)
+    print("Factorial: ",fact)
