2ce2b61f4565db2c5f91513e8f810799dfa8e606
SalvadorDuenas <89178506+SalvadorDuenas@users.noreply.github.com>
2022-09-06 19:00:27 -0500

    Update 19011235.py

--- 0829/19011235.py
+++ 0829/19011235.py
@@ -8,18 +8,26 @@
 # No. Control: 19011235
 # Calificación: XXX
 
+# Definimos la funcion factorial
 def factorial(n):
-    if n >= 0:
+    #Creamos un if si "n" es mayor a 0
+    if n > 0:
         num = 0
         fact = 1
+        #Creamos un for para el numero en en cuestion a evaluar 
         for num in range(1, n, 1):
             fact *= n
             n-=1
         return fact
+    #elif si "n" es 0
+    elif n == 1:
+        return 1
+     #else si "n" menor o diferente de 0
     else:
         cadena = "invalido, ingrese numeros positivos"
         return cadena
-        
+
+#Pedimos al usuario que digite la entrada
 numero = int(input("Por favor ingrese un valor:"))
 print ("El factorial de",numero,"es",factorial(numero))
 
