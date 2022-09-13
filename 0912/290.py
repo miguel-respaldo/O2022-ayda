ec87391749ce6384b3a7679054e7da8f5bd85c25
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2022-09-12 19:50:50 -0500

    iniciamos fibonacci

---
+++ 0912/290.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+#
+# Nombre: Nombre del Estudiante
+# No. Control: 123456789
+# Calificación: XXX
+
+def fibonacci(num):
+    # Modificar esta parte
+    return num**2
+
+
+def main():
+    numero = eval(input("Escribe un número: "))
+    print("La posición", numero, "de fibonacci es", fibonacci(numero))
+
+
+if __name__ == "__main__":
+    main()
+
