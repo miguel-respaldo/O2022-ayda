547060de3e1c4128d40f449d34247f7c0766f522
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2022-09-13 19:28:42 -0500

    los martes

---
+++ 0913/290.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+#
+# Nombre: Nombre del Estudiante
+# No. Control: 123456789
+# Calificación: XXX
+
+def fibonacci(num):
+    # Tomando en cuenta el 0 como posición 1
+    # Modificar esta parte
+    return num**2 # y esta linea
+
+
+def main():
+    numero = eval(input("Escribe un número: "))
+    print("La posición", numero, "de fibonacci es", fibonacci(numero))
+
+
+if __name__ == "__main__":
+    main()
+
