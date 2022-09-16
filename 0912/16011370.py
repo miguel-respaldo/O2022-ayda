ee98b4e1fc55c8380ca52ec82d7652b183cf296d
luis Rubio <luisf.rubio1997@gmail.com>
2022-09-16 12:56:43 -0500

    16011370 fibonacci

---
+++ 0912/16011370.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+#
+# Nombre: Nombre del Estudiante
+# No. Control: 123456789
+# Calificación: XXX
+
+def fibonacci(num):
+    # Tomando en cuenta el 0 como posición 1
+    # Modificar esta parte
+   # return num**2  y esta linea
+    if num < 2:
+        return num
+    else:
+        return fibonacci(num-1) + fibonacci(num - 2)
+
+def main():
+    numero = eval(input("Escribe un número: "))
+    print("La posición", numero, "de fibonacci es", fibonacci(numero))
+
+
+if __name__ == "__main__":
+    main()
+
