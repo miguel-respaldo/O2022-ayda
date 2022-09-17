75e767b50ef84e25ab2314f9a07ba91602248cba
Zenitramhgx <85717522+Zenitramhgx@users.noreply.github.com>
2022-09-16 23:52:16 -0500

    Add files via upload

---
+++ 0913/19011240.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+#
+# Nombre: Hugo Alberto Gonzalez Martinez
+# No. Control: 19011240
+# Calificación: XXX
+
+def fibonacci(num):
+    
+    if(num == 0):
+        return 0
+    elif (num == 1):
+        return 1
+    else:
+        return fibonacci(num -1) + fibonacci(num -2)
+    return num**2 
+
+
+def main():
+    numero = eval(input("Escribe un número: "))
+    print("La posición", numero, "de fibonacci es", fibonacci(numero))
+
+
+if __name__ == "__main__":
+    main()
