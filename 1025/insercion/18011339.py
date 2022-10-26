8a22fe496e1abd2209cdd9d13612d1692764db9c
Nestor7626 <111822586+Nestor7626@users.noreply.github.com>
2022-10-25 21:03:30 -0500

    Add files via upload

---
+++ 1025/insercion/18011339.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+#
+# Nombre: Hernandez Uribe Nestor Eduardo
+# No. Control: 18011339
+# Calificación: XXX
+
+def insercion(n):
+    # Longitud del arreglo
+    for i in range(1, len(n)):
+
+        guarda = n[i]
+        #indice del elemento anterior
+        j = i - 1
+        # En funcion del valor seran transladados
+        while j >= 0 and n[j] > guarda:
+
+            n[j + 1] = n[j]
+            j -= 1
+            
+        # Inserta el nuevo valor
+        n[j + 1] = guarda
+
+
+randomNumbers = [9, 1, 91, 19, 38, 26, 67]
+# Ingresar el arreglo a la funcion
+insercion(randomNumbers)
+print(randomNumbers)
