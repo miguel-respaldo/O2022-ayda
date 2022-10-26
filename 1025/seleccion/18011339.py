a2e8eb953f1d8584f6beb280cb86b50f463374a5
Nestor7626 <111822586+Nestor7626@users.noreply.github.com>
2022-10-25 21:05:27 -0500

    Add files via upload

---
+++ 1025/seleccion/18011339.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+#
+# Nombre: Hernandez Uribe Nestor Eduardo
+# No. Control: 18011339
+# Calificación: XXX
+
+def sel(arreglo):
+    for m in range(len(arreglo) - 1): 
+        h = m
+        for j in range(m + 1, len(arreglo)):
+            if arreglo[j] < arreglo[h]:
+                h = j
+        if h != m:
+            arreglo[h], arreglo[m] = arreglo[m], arreglo[h]
+
+valores = [5,45,78,1,64,65,8]
+sel(valores)
+
+print(valores)
