5b61e0351134e3811359949b8e24222b5cfcc7c5
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2022-11-15 19:37:00 -0600

    tarea

---
+++ 1115/quicksort/290.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+#
+# Nombre: Nombre del Estudiante
+# No. Control: 123456789
+# Calificación: XXX
+
+def funcion_de_tarea(num):
+    return num*2
+
+
+def main():
+    print("Este es el main y llama la función de tarea")
+    resultado = funcion_de_tarea(2)
+
+
+if __name__ == "__main__":
+    main()
