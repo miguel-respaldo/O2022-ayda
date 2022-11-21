7d6ab3769f3bbeb72dacd824d5e501f4d8e4f034
Josue Rocha <josue.rocha0809@gmail.com>
2022-11-21 17:42:17 -0600

    18011187 tarea

--- 1115/mergesort/18011187.py
+++ 1115/mergesort/18011187.py
@@ -11,30 +11,22 @@ def mergesort(arr):
         left = arr[:mid]
         right = arr[mid:]
 
-        # Recursive call on each half
         mergesort(left)
         mergesort(right)
 
-        # Two iterators for traversing the two halves
         i = 0
         j = 0
-
-        # Iterator for the main list
         k = 0
 
         while i < len(left) and j < len(right):
             if left[i] <= right[j]:
-                # The value from the left half has been used
                 arr[k] = left[i]
-                # Move the iterator forward
                 i += 1
             else:
                 arr[k] = right[j]
                 j += 1
-            # Move to the next slot
             k += 1
 
-        # For all the remaining values
         while i < len(left):
             arr[k] = left[i]
             i += 1
