93c668441c4f365bd014bdeea37bd7a1416e1724
Nestor7626 <111822586+Nestor7626@users.noreply.github.com>
2022-11-27 00:40:58 -0600

    Add files via upload

---
+++ 1115/mergesort/18011339.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+#
+# Nombre: Nestor Edaurdo Hernandez Uribe
+# No. Control: 18011339
+# Calificación: XXX
+
+def ordenamientoPorMezcla(unaLista):
+    print("Dividir ",unaLista)
+    if len(unaLista)>1:
+        mitad = len(unaLista)//2
+        mitadIzquierda = unaLista[:mitad]
+        mitadDerecha = unaLista[mitad:]
+
+        ordenamientoPorMezcla(mitadIzquierda)
+        ordenamientoPorMezcla(mitadDerecha)
+
+        i=0
+        j=0
+        k=0
+        while i < len(mitadIzquierda) and j < len(mitadDerecha):
+            if mitadIzquierda[i] < mitadDerecha[j]:
+                unaLista[k]=mitadIzquierda[i]
+                i=i+1
+            else:
+                unaLista[k]=mitadDerecha[j]
+                j=j+1
+            k=k+1
+
+        while i < len(mitadIzquierda):
+            unaLista[k]=mitadIzquierda[i]
+            i=i+1
+            k=k+1
+
+        while j < len(mitadDerecha):
+            unaLista[k]=mitadDerecha[j]
+            j=j+1
+            k=k+1
+    print("Mezclar ",unaLista)
+
+unaLista = [54,26,93,17,77,31,44,55,20]
+ordenamientoPorMezcla(unaLista)
+print(unaLista)
